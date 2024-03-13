4122a1563d1f65cffaab8775b8dc2cf855021af9
pandu <srinuvinaykumar.narigiri@gmail.com>
2024-03-13 23:00:18 +0530

    mar13

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -44,30 +44,19 @@ const LoginScreen = () => {
       <Header />
       <Row className="px-5 pt-4 bodydiv">
         <Col className="col-lg-6 col-md-12 col-sm-12 col-xs-12 ">
-<<<<<<< HEAD
           <Form className="p-4 border border-dark rounded mx-5 ">
             <h1 style={{ color: "#1B1A55", fontWeight: "750" }}>Login</h1>
             <Row className="py-3">
               <Col style={{ fontWeight: "700" }}>
-=======
-          <Form  className="p-4 border border-dark rounded mx-5 ">
-            <h1 style={{fontWeight:"600",color:"#1B1A55",fontWeight:"750"}}>Login</h1>
-            <Row className="py-3">
-              <Col style={{fontWeight:"700"}}>
->>>>>>> 536055118085a1e3b37775d7348c21ace9ee32db
                 Doesn't have an account yet? <Link to="/register">Signup</Link>{" "}
               </Col>
             </Row>
 
             <Form onSubmit={submitHandler}>
               <Form.Group className="my-2" controlId="empId">
-<<<<<<< HEAD
                 <Form.Label style={{ fontWeight: "500", color: "black" }}>
                   Employee Id
                 </Form.Label>
-=======
-                <Form.Label style={{fontWeight:"500",color:"black"}}>Employee Id</Form.Label>
->>>>>>> 536055118085a1e3b37775d7348c21ace9ee32db
                 <Form.Control
                   type="number"
                   placeholder="1235"
@@ -77,20 +66,12 @@ const LoginScreen = () => {
               </Form.Group>
 
               <Form.Group className="my-2" controlId="password">
-<<<<<<< HEAD
                 <div className="pwd">
                   <Form.Label style={{ fontWeight: "500", color: "black" }}>
                     Password
                   </Form.Label>
                   <ForgotPassword />
                 </div>
-=======
-                <div className="pwd" >
-                <Form.Label style={{fontWeight:"500",color:"black"}}>Password</Form.Label>
-                <Form.Label style={{fontWeight:"500",color:"black"}}>Forgot Password</Form.Label>
-                </div>
-                
->>>>>>> 536055118085a1e3b37775d7348c21ace9ee32db
 
                 <Form.Control
                   type="password"
@@ -102,27 +83,19 @@ const LoginScreen = () => {
 
               {isLoading && <Loader />}
 
-<<<<<<< HEAD
               <Button
                 type="submit"
                 style={{ color: "white", background: "#1B1A55", width: "100%" }}
                 onClick={submitHandler}
                 className="mt-3 "
               >
-=======
-              <Button type="submit" style={{color:"white",background:"#1B1A55",width:"100%"}} className="mt-3 ">
->>>>>>> 536055118085a1e3b37775d7348c21ace9ee32db
                 Login
               </Button>
             </Form>
           </Form>
         </Col>
         <Col className="col-lg-6 col-md-12 col-sm-12 col-xs-12 pt-4 ">
-<<<<<<< HEAD
           <img className="img-fluid" src={loginImage} alt="Login Page" />
-=======
-          <img className="img-fluid" src={loginImage} alt="Login Page"/>
->>>>>>> 536055118085a1e3b37775d7348c21ace9ee32db
         </Col>
       </Row>
     </>
